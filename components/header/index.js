b3281503c9593687d5562e9791c8a936f840c980
kataldo <kataldo@MacBook-Air-de-kataldo.local>
2022-06-15 01:31:10 -0400

    Header terminado

---
+++ components/header/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { View, Text, SafeAreaView } from 'react-native';
+import { styles } from './styles';
+
+const Header = ({ title }) => {
+  return (
+    <SafeAreaView style={styles.container}>
+
+<View style={styles.container}>
+      <Text style={styles.title}>{title}</Text>
+    </View>
+    </SafeAreaView>
+   
+  );
+};
+
+export default Header;
---
+++ components/header/styles.js
@@ -0,0 +1,17 @@
+import {StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create({
+    container:{
+width:'100%',
+height:140,
+backgroundColor:'#5870D8',
+alignItems:'center',
+justifyContent:'center'
+    },
+    title:{
+color:'#fff',
+fontSize:22,
+textTransform:'uppercase',
+paddingTop: 50,
+    }
+})
